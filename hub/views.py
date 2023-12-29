2593136446c6e3bbe2313cbe3d4349db0e94f41b
MacBook <macbook@MacBook-Pro--Nikita.local>
2023-12-29 17:47:17 +0300

    Фикстуры

--- hub/views.py
+++ hub/views.py
@@ -1,18 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import Categories
 
 
 def index(request):
+    categories = Categories.objects.all()
+
     context = {'title': 'home',
                'content': 'Магазин мебели HOME',
-               'list': ['1', 1, [2]],
-               'dict': {3: 4},
-               'bool': False}
+               'categories': categories,
+               }
     return render(request, 'hub/index.html', context)
 
 
 def about(request):
-    context = dict(title='О нас', content='О нас',
+    context = dict(title='О нас',
+                   content='О нас',
                    text_on_page="Много много много текста о нас, какие мы крутые, что нас отличает,"
                                 "какие мы все тут особенные и классные")
     return render(request, 'hub/about.html', context)
