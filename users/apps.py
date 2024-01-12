875c9b3ee2b6f4c3dcd00a3d6f5caa4473cbcf49
macbook <halk53400@gmail.com>
2024-01-12 15:32:57 +0300

    Login form ready

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+    verbose_name = 'Пользователи'
+
+    class Meta:
+        db_table = "user"
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
--- users/views.py
+++ users/views.py
@@ -1,13 +1,28 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from users.forms import UserLoginForm
+
 
 def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('hub:index'))
+    else:
+        form = UserLoginForm()
 
     context = {
         'title': 'Home - Авторизация',
+        'form': form,
         # 'form': form
     }
     return render(request, 'login.html', context)
